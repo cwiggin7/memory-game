5e39eaf9d0ee0cbbf47350c1767d06311a90e979
cwiggin7 <cwiggin7@ksu.edu>
2023-08-19 19:40:28 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">Card 1</div>
+      <div className="card">Card 2</div>
+      <div className="card">Card 3</div>
+      <div className="card">Card 4</div>
+      <div className="card">Card 5</div>
+      <div className="card">Card 6</div>
+    </div>
+  );
+}
+
+export default App;
