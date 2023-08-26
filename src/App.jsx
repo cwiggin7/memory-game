12f84a749d4571d1db6d692580566ba9f5f4ee47
cwiggin7 <cwiggin7@ksu.edu>
2023-08-25 22:55:37 -0500

    fetching pokemon data from pokeapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  const handleCardClick = () => {};
+
+  useEffect(() => {
+    const offsetNumber = Math.floor(Math.random() * 1276);
+
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=6&offset=${offsetNumber}`)
+      .then((resp) => resp.json())
+      .then((result) => {
+        // Map through the results and fetch data from each URL
+        const fetchPromises = result.results.map((pokemon) =>
+          fetch(pokemon.url)
+            .then((resp) => resp.json())
+            .then(({ name, sprites }) => ({
+              name,
+              sprite: sprites.front_default,
+            }))
+        );
+
+        // Resolve all the fetch promises
+        Promise.all(fetchPromises).then((data) => {
+          setPokemonData(data); // Update the state with fetched data
+        });
+      });
+  }, []);
+
   return (
     <div className="container">
-      <div className="card">Card 1</div>
-      <div className="card">Card 2</div>
-      <div className="card">Card 3</div>
-      <div className="card">Card 4</div>
-      <div className="card">Card 5</div>
-      <div className="card">Card 6</div>
+      {pokemonData.map((pokemon, index) => (
+        <div className="card" key={index} onClick={handleCardClick}>
+          <p>{pokemon.name}</p>
+          <img src={pokemon.sprite} alt={`${pokemon.name} sprite`} />
+        </div>
+      ))}
     </div>
   );
 }
